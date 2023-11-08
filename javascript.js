f0f99e1f1f943d91183b07c12c5fde7d174c2174
ilopez000 <ignacio.lopez.aylagas@gmail.com>
2023-11-08 16:46:14 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
   // Seleccionamos el botón por su ID
   const boton = document.getElementById('botonCambioColor');
 
-  // Añadimos un event listener al botón para que cuando se haga clic ejecute una función
-  boton.addEventListener('click', function() {
+    // Añadimos un event listener al botón para que cuando se haga clic ejecute una función
+    boton.addEventListener('click', function() {
     // Seleccionamos todos los párrafos
     document.getElementById('parrafo3').style.color = 'red';
-    parrafo.style.color = 'red';
-   
   });
